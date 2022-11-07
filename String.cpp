463bf83e9b0a056fe868a6c62e086c8f23d778e8
Ruslan <ruslan56781@mail.ru>
2022-11-08 00:08:12 +0300

    add String class

--- String.cpp
+++ String.cpp
@@ -2,13 +2,74 @@
 #include "String.hpp"
 #include <stdexcept>
 
+String::String()
+{
+   m_str=new char[1];
+   m_len=0;
+   m_cap=0;
+}
+
 String::String(const char *_str)
 {
     m_len=strlen(_str);
-    m_str=new char[m_len];
+    m_str=new char[m_len+1];
     strncpy(m_str,_str,m_len);
 }
 
+
+String::String(const char *_str, std::size_t t_sym)
+{
+   if(t_sym<0)
+      throw std::out_of_range("Wrong input index line !");
+
+   m_len=strlen(_str);
+   m_str=new char[m_len+1];
+   strncpy(m_str,_str,t_sym);
+}
+
+String::String(const String &_other)
+{
+    strncpy(m_str,_other.m_str,_other.m_len);
+    m_len=_other.m_len;
+    m_cap=_other.m_cap;
+}
+
+String::String(String &&rhs)
+{
+    strncpy(m_str,rhs.m_str,m_len);
+    m_cap=rhs.m_cap;
+    m_len=rhs.m_len;
+    m_str=nullptr;
+}
+
+String &String::operator=(const String &rhs)
+{
+  if(this!=&rhs){
+      delete [] m_str;
+
+      m_str=new char[rhs.m_len+1];
+      strncpy(m_str,rhs.m_str,rhs.m_len);
+      m_len=rhs.m_len;
+      m_cap=rhs.m_cap;
+  }
+
+  return *this;
+}
+
+String & String::operator=(String &&rhs)
+{
+    if(this!=&rhs){
+        delete [] m_str;
+
+        m_str=new char[rhs.m_len+1];
+        strncpy(m_str,rhs.m_str,rhs.m_len);
+        m_len=rhs.m_len;
+        m_cap=rhs.m_cap;
+        m_str=nullptr;
+    }
+
+    return *this;
+}
 
 String &String::push_back(char m_sym)
 {
@@ -16,10 +77,20 @@ String &String::push_back(char m_sym)
     return *this;
 }
 
+String &String::pop_back()
+{
+    if(empty())
+        throw std::logic_error("String empty ");
+
+    m_str[m_len]=m_str[m_len--];
+
+    return *this;
+}
+
 String &String::append(const char *_str)
 {
     m_len=strlen(_str);
-    m_str=new char[m_len];
+    m_str=new char[m_len+1];
     strncpy(m_str,_str,m_len);
 
     return *this;
@@ -27,10 +98,20 @@ String &String::append(const char *_str)
 
 String &String::append(const char *_str, std::size_t _size)
 {
-    m_len=std::move(_size);
+    m_len=strlen(_str);
 
-    m_str=new char[_size];
+    m_str=new char[_size+1];
     strncpy(m_str,_str,_size);
+
+    return *this;
+}
+
+String &String::insert(std::size_t _pos, const char *_str)
+{
+    if(_pos<0 && _pos<m_len)
+        throw std::logic_error("Wrong input position !");
+
+   strncat(m_str,_str,strlen(_str));
 
     return *this;
 }
@@ -66,14 +147,14 @@ bool operator==(const String &_hrs,const String & _rhs)
 
 }
 
-char &String::operator[](std::size_t t_size)
+char &String::operator[](std::size_t t_index)
 {
-    return *(m_str+t_size);
+    return *(m_str+t_index);
 }
 
-const char &String::operator[](std::size_t t_size)const
+const char &String::operator[](std::size_t t_index)const
 {
-       return *(m_str+t_size);
+       return *(m_str+t_index);
 }
 
 const char &String::back() const
@@ -100,13 +181,30 @@ char &String::front()
 char &String::at(std::size_t t_index)
 {
     if(t_index<0 && t_index>m_len)
-        throw std::out_of_range("Wrong input index line");
+        throw std::logic_error("Wrong input index line");
+    return operator[](t_index);
+}
+
+const char *String::c_str() const
+{
+    return m_str;
+}
+
+const char &String::at(std::size_t t_index) const
+{
+    if(t_index<0 && t_index>m_len)
+        throw std::logic_error("Wrong input index line");
     return operator[](t_index);
 }
 
 std::size_t String::size() const
 {
-    return strlen(m_str);
+    return m_len;
+}
+
+std::size_t String::max_size() const
+{
+    return String::npos;
 }
 
 std::size_t String::length() const
@@ -114,47 +212,44 @@ std::size_t String::length() const
     return m_len;
 }
 
+std::size_t String::capacity() const
+{
+    return m_cap;
+}
+
 bool String::empty() const
 {
     return m_len ? false:true;
 }
 
-const char &String::at(std::size_t t_index) const
+void String::clear()
 {
-    if(t_index<0 && t_index>m_len)
-        throw std::out_of_range("Wrong input index line");
-    return operator[](t_index);
-}
-
-String::String(const String &_other)
-{
-    m_str=_other.m_str;
-    m_len=_other.m_len;
-    m_cap=_other.m_cap;
-}
-
-String::String(String &&rhs)
-{
-   *this=std::move(rhs);
-}
-
-String &String::operator=(const String &rhs)
-{
-    if(this!=&rhs){
-        rhs.~String();
-      *this=operator=(rhs);
-   }
-    return *this;
-}
-
-String & String::operator=(String &&rhs)
-{
-    if(this!=&rhs){
-        rhs.~String();
-        *this=operator=(std::move(rhs));
+    for(std::size_t i=0;i<m_len;i++){
+        m_str[i]=0;
     }
+    m_cap=0;
+    m_len=0;
+}
 
-    return *this;
+void String::resize(std::size_t _size)
+{
+    if(m_len==_size)
+        return;
+
+   m_len=_size;
+   char *_temp=new char[m_len+1];
+
+   strncpy(_temp,m_str,m_len);
+   delete [] m_str;
+   m_str=_temp;
+}
+
+String::~String()
+{
+    delete [] m_str;
+    m_str=nullptr;
+    m_len=0;
+    m_cap=0;
 }
 
 std::ostream & operator<<(std::ostream & out,const String &_str)
@@ -172,23 +267,16 @@ std::istream & operator>>(std::istream &in,String &_str)
         throw std::runtime_error("Error on input !");
     }
 
-    char m_sym;
+   char m_sym;
 
-    while(true){
-        m_sym=in.get();
+   while(true){
+       in.get(m_sym);
 
-        if(m_sym=='\n' && m_sym==' ')
-            break;
+       if(std::isspace(m_sym,in.getloc())|| in.eof())
+             break;
 
         _str.push_back(m_sym);
-    }
+   }
 
     return in;
-}
-
-String::~String()
-{
-    delete  m_str;
-    m_len=0;
-    m_cap=0;
 }
--- String.hpp
+++ String.hpp
@@ -7,7 +7,7 @@
 
 class String{
 public:
-    String() = default ;
+    String() ;
 
     String(const String &);
 
@@ -22,12 +22,18 @@ public:
 public:
     String(const char *_str);
 
+    String(const char *_str,std::size_t t_sym);
+
     String & push_back(char m_sym);
+
+    String & pop_back();
 
 public:
     String &append(const char *_str);
 
     String & append(const char *_str,std::size_t _size);
+
+    String &insert(std::size_t _pos,const char *_str);
 
 public:
     friend  bool operator<(const String &_hrs,const String &_rhs);
@@ -48,8 +54,8 @@ public:
     friend std::istream & operator>>(std::istream &in,String &_str);
 
 public:
-    const char &operator[](std::size_t t_size)const;
-    char &operator[](std::size_t t_size);
+    const char &operator[](std::size_t t_index)const;
+    char &operator[](std::size_t t_index);
 
     const char &back()const;
     char &back();
@@ -60,15 +66,29 @@ public:
     const char &at(std::size_t t_index)const;
     char &at(std::size_t t_index);
 
+    const char *c_str()const;
+
 public:
    std::size_t size()const;
 
+   std::size_t max_size()const;
+
    std::size_t length()const;
+
+   std::size_t capacity()const;
 
    bool empty()const;
 
+public:
+   void clear();
+
+
+   void resize(std::size_t _size);
+
+   void reverse(std::size_t _size);
+
 private:
-    char * m_str = nullptr; // c - string
+    char * m_str; // c - string
     std::size_t m_len = 0; // length
     std::size_t m_cap= 0; // capacity
 
